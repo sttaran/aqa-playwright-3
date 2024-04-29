8a1e702c898e9fba60b0a1411fc854afb5ccffa1
staran <staran@griddynamics.com>
2024-04-29 20:52:19 +0300

    staran // fix API test

--- tests/api/jsonplaceholder/test.spec.js
+++ tests/api/jsonplaceholder/test.spec.js
@@ -5,5 +5,5 @@ test("test", async ({request})=>{
   const response = await request.get("https://jsonplaceholder.typicode.com/posts")
   const body = await response.json()
   expect(response.status()).toBe(200)
-  expect(body.length).toBeLessThan(100)
+  expect(body.length).toBe(100)
 })
